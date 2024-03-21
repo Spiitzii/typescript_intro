1b353f9fa8b262041a20352ffb4bfb1e2507a48e
Spiitzii <spitzi@online.de>
2024-03-21 12:58:38 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("heute ist Donnerstag");
+console.log("tsc -w app.ts");
+// Variablen deklarieren
+// indirekt
+var trainer = 'Lucas';
+//  direkt
+var tag;
+// var ist böse
+// var getränk = 'Kaffee'
+tag = 'Freitag';
+// tag = 4
+console.log(tag);
+function veroppeln(zahl) {
+    return zahl * 2;
+}
+console.log(veroppeln(15));
---
+++ app.ts
@@ -0,0 +1,29 @@
+console.log("heute ist Donnerstag")
+console.log("tsc -w app.ts")
+
+// Variablen deklarieren
+
+// indirekt
+let trainer = 'Lucas'
+
+//  direkt
+let tag: string
+
+// var ist böse
+// var getränk = 'Kaffee'
+
+tag = 'Freitag'
+// tag = 4
+
+console.log(tag)
+
+function veroppeln(zahl: number) {
+    return zahl * 2
+    
+
+}
+
+console.log(veroppeln(15))
+
+
+
